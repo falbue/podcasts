628248a1a93c62a7f9fe3aee6a5180890c01aaf3
Falbue <cyansair05@gmail.com>
2024-12-25 19:24:58 +0500

    Добавлены команды

--- bot.py
+++ bot.py
@@ -9,6 +9,12 @@ print(VERSION)
 
 bot = telebot.TeleBot(config.API)
 
+commands = [  # КОМАНДЫ
+telebot.types.BotCommand("start", "Перезапуск"),
+telebot.types.BotCommand("help", "Помощь"),
+]
+bot.set_my_commands(commands)
+
 
 @bot.message_handler(commands=['start'])  # обработка команды start
 def start(message):
@@ -20,8 +26,17 @@ def start(message):
     if menu_id:
         try:
             bot.delete_message(message.chat.id, menu_id)
-        except apihelper.ApiTelegramException:
+        except:
             pass
+
+@bot.message_handler(commands=['help'])  # обработка команды start
+def help(message):
+    user_id = message.chat.id
+    menu_id = SQL_request("SELECT message FROM users WHERE id = ?", (user_id,))
+    text = "Текст с помощью"
+    keyboard = create_keyboard.help()
+    bot.edit_message_text(chat_id=user_id, message_id=menu_id, text=text, reply_markup=keyboard)
+    bot.delete_message(message.chat.id, message.message_id)
 
 @bot.message_handler(func=lambda message: True)
 def handle_text_message(message):
