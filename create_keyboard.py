de3b98dcbd5db2c34a97e9d03ddea81a4fd9b57b
Falbue <cyansair05@gmail.com>
2024-12-25 19:25:07 +0500

    Клавиатура для помощи

--- create_keyboard.py
+++ create_keyboard.py
@@ -8,3 +8,9 @@ def main():
     btn_random = InlineKeyboardButton("Случайный", callback_data='random')
     keyboard.add(btn_save, btn_random)
     return keyboard
+
+def help():
+    keyboard = InlineKeyboardMarkup(row_width=2)
+    btn_return = InlineKeyboardButton("< Назад", callback_data='return:main')
+    keyboard.add(btn_return)
+    return keyboard
